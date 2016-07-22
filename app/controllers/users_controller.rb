acf5d98b851c3a2b430096917aad596742db13a2
Bill Patrianakos <bill@billpatrianakos.me>
2016-07-22 12:00:49 -0500

    Add all 4 CRUD methods in the users controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
+  get '/update/:username/:new_username/?' do |username, new_username|
+    user = User.find_by username: username
+
+    if user
+      user.username = new_username
+      user.save
+      "User #{username} was changed to #{user.username}"
+    else
+      "No user to update"
+    end
+  end
+
+  get '/delete/:username/?' do |username|
+    user = User.find_by username: username
+
+    if user
+      user.destroy
+      "Just destroyed a user."
+    else
+      "Can't destroy a thing that doesn't exist"
+    end
+  end
+
   get '/:username/:password/?' do |username, password|
     if username != '' && password != ''
       user = User.create username: username, password: password
@@ -8,6 +31,16 @@ class UsersController < ApplicationController
       'User was created'
     else
       'There was an error creating a user'
+    end
+  end
+
+  get '/:username/?' do |username|
+    user = User.find_by username: username
+
+    if user
+      "Your username was #{user.username}"
+    else
+      'No user by that name found'
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160721192419) do
+ActiveRecord::Schema.define(version: 20160722155228) do
+
+  create_table "todos", force: :cascade do |t|
+    t.string "content"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
